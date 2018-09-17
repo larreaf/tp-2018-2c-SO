c07a834ee8f7f2493d5cf4e99a5b0342a1d41486
santi871 <santivegega@gmail.com>
2018-09-17 17:50:25 -0300

    creo servidor como en mdj

--- fm9/fm9.c
+++ fm9/fm9.c
@@ -10,13 +10,66 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <ensalada/validacion.h>
+#include "validacion.h"
+#include "com.h"
+#include <commons/log.h>
+#include <sys/select.h>
+#include <unistd.h>
+#include <signal.h>
+#include <servidor.h>
+#include "protocolo.h"
+#include "servidor.h"
+#include <string.h>
+
+void cerrar_fm9(t_log* logger, cfg_fm9* configuracion, Servidor server){
+    log_info(logger, "Cerrando FM9...");
+
+    // destruir_servidor manda headers CONEXION_CERRADA a todos los clientes conectados para que se enteren y despues
+    // cierra cada socket
+    destruir_servidor(server);
+    log_destroy(logger);
+    destroy_cfg(configuracion, t_fm9);
+    exit(0);
+}
 
 int main(int argc, char **argv) {
-	validar_parametros(argc);
-	t_config* cfg_file = validar_config(argv[1],fm9);
-	cfg_fm9* configuracion = asignar_config(cfg_file,fm9);
+    int conexiones_permitidas[cantidad_tipos_procesos] = {0};
+    char* str;
+    MensajeEntrante mensaje;
+    Servidor server;
 
-	puts(""); /* prints  */
-	return EXIT_SUCCESS;
+    t_log *logger = log_create("fm9.log", "fm9", true, log_level_from_string("info"));
+
+    log_info(logger, "Inicializando config...");
+    validar_parametros(argc);
+    cfg_fm9 *configuracion = asignar_config(argv[1], fm9);
+
+    log_info(logger, "Inicializando socket server...");
+    conexiones_permitidas[t_cpu] = 2;
+    conexiones_permitidas[elDiego] = 1;
+    server = inicializar_servidor(logger, configuracion->puerto, conexiones_permitidas, t_fm9);
+
+    while (1){
+        mensaje = esperar_mensajes(server);
+
+        switch (mensaje.header) {
+            case STRING_DIEGO_FM9:
+                str = recibir_string(mensaje.socket);
+                printf("Recibio: %s\n", str);
+
+                if(!strcmp(str, "exit"))
+                    cerrar_fm9(logger, configuracion, server);
+                
+                break;
+                
+            case CONEXION_CERRADA:
+                // ejemplo si se cierra elDiego y nos manda un header CONEXION_CERRADA, fm9 se cierra tambien
+                
+                if(mensaje.t_proceso==t_elDiego)
+                    cerrar_fm9(logger, configuracion, server);
+
+            default:
+                break;
+        }
+    }
 }

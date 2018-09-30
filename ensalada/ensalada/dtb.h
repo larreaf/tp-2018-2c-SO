bf446c46bed34974192648bb0cb270bd3760f8f1
santi871 <santivegega@gmail.com>
2018-09-30 19:41:16 -0300

    agrego diccionario de archivos abiertos

--- ensalada/ensalada/dtb.h
+++ ensalada/ensalada/dtb.h
@@ -1,12 +1,14 @@
 #ifndef DTB_H
 #define DTB_H
 
+#include <commons/collections/list.h>
+
 typedef struct{
     int id;
     char* path_script;
     int program_counter;
     int inicializado;
-    //TODO agregar tabla de archivos abiertos
+    t_dictionary* archivos_abiertos;
 }DTB;
 
 #endif //DTB_H

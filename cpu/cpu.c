e7c5ec5023715642298bdcb816827b6ccb75d721
santi871 <santivegega@gmail.com>
2018-10-06 00:34:45 -0300

    CPU puede recibir/devolver datos de un DTB y ejecutar instrucciones

--- cpu/cpu.c
+++ cpu/cpu.c
@@ -6,7 +6,13 @@
 #include <ensalada/protocolo.h>
 #include <ensalada/validacion.h>
 #include <ensalada/com.h>
+#include <ensalada/mensaje.h>
 #include <ensalada/servidor.h>
+#include <ensalada/dtb.h>
+#include "parser.h"
+
+ConexionesActivas conexiones_activas;
+t_log* logger;
 
 void cerrar_cpu(t_log* logger, cfg_cpu* configuracion, ConexionesActivas conexiones_activas){
     log_info(logger, "Cerrando CPU...");
@@ -20,11 +26,12 @@ void cerrar_cpu(t_log* logger, cfg_cpu* configuracion, ConexionesActivas conexio
 }
 
 int main(int argc, char **argv) {
-	int socket_fm9, socket_safa, socket_elDiego, header;
+	int socket_fm9, socket_safa, socket_elDiego;
 	int conexiones_permitidas[cantidad_tipos_procesos] = {0};
-	ConexionesActivas conexiones_activas;
-	MensajeEntrante mensaje;
-    t_log* logger;
+    t_accion_post_instruccion resultado_instruccion = READY;
+	DTB datos_dtb;
+	MensajeDinamico *peticion_abrir_script, *mensaje;
+    char* linea = "";
 
     logger = log_create("cpu.log", "cpu", true, log_level_from_string("info"));
 
@@ -39,7 +46,8 @@ int main(int argc, char **argv) {
     socket_safa = conectar_como_cliente(conexiones_activas, configuracion->ip_safa, configuracion->puerto_safa, t_safa);
     log_info(logger, "Conexion con s-AFA lista");
 
-    socket_elDiego = conectar_como_cliente(conexiones_activas, configuracion->ip_elDiego, configuracion->puerto_elDiego, t_elDiego);
+    socket_elDiego = conectar_como_cliente(conexiones_activas, configuracion->ip_elDiego, configuracion->puerto_elDiego,
+            t_elDiego);
     log_info(logger, "Conexion con elDiego lista");
 
     log_info(logger, "Listo");
@@ -47,9 +55,71 @@ int main(int argc, char **argv) {
 	while(true){
 	    mensaje = esperar_mensajes(conexiones_activas);
 
-	    switch(mensaje.header){
+	    switch(mensaje->header){
+
+	        case DATOS_DTB:
+	            desempaquetar_dtb(mensaje, &datos_dtb);
+                log_info(logger, "Datos DTB entrantes de DTB %d...", datos_dtb.id);
+
+	            switch(datos_dtb.inicializado){
+                    case 0:
+                        // es el DTB dummy
+
+                        log_info(logger, "Ejecutando DTB dummy, enviando peticion abrir %s", datos_dtb.path_script);
+                        peticion_abrir_script = crear_mensaje(ABRIR_SCRIPT_CPU_DIEGO, socket_elDiego);
+                        agregar_dato(peticion_abrir_script, sizeof(int), &datos_dtb.id);
+                        agregar_string(peticion_abrir_script, datos_dtb.path_script);
+                        enviar_mensaje(peticion_abrir_script);
+                        datos_dtb.status = BLOQUEAR;
+                        enviar_datos_dtb(socket_safa, &datos_dtb);
+
+                        resultado_instruccion = BLOQUEAR;
+                        break;
+
+	                case 1:
+	                    // es un DTB comun
+
+	                    while(resultado_instruccion == READY) {
+                            // TODO ir a buscar la linea indicada por el program counter y ejecutarla
+                            linea = "";
+
+                            // los comentarios comienzan con #
+                            if(string_starts_with(linea, "#"))
+                                continue;
+
+                            resultado_instruccion = ejecutar_linea(&datos_dtb, linea,
+                                    (unsigned int) configuracion->retardo);
+
+                            datos_dtb.program_counter++;
+                        }
+
+                        if(resultado_instruccion == -1){
+                            log_error(logger, "Cantidad de argumentos erroneos en DTB %d, linea: %s\n", datos_dtb.id,
+                                    linea);
+                            
+                            resultado_instruccion = DTB_EXIT;
+                        }
+                        else if(resultado_instruccion == -2){
+                            log_error(logger, "Operacion no reconocida en DTB %d, linea: %s\n", datos_dtb.id,
+                                      linea);
+
+                            resultado_instruccion = DTB_EXIT;
+                        }
+                            
+	                    datos_dtb.status = resultado_instruccion;
+	                    enviar_datos_dtb(socket_safa, &datos_dtb);
+                        break;
+
+                    default:
+                        log_error(logger, "Flag inicializado de DTB %d invalida, abortando DTB...", datos_dtb.id);
+                        resultado_instruccion = DTB_EXIT;
+                        break;
+	            }
+
+	            break;
+
             case CONEXION_CERRADA:
-                switch(mensaje.t_proceso){
+                switch(mensaje->t_proceso){
                     case t_safa:
                         log_warning(logger, "CPU perdio conexion con SAFA, cerrando CPU");
                         cerrar_cpu(logger, configuracion, conexiones_activas);
@@ -65,6 +135,12 @@ int main(int argc, char **argv) {
                     default:
                         break;
                 }
+
+            default:
+                // header invalido
+                break;
 	    }
+
+	    destruir_mensaje(mensaje);
 	}
 }

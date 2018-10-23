da799a5ead1b9b667d790e5260effee186f00b40
santi871 <santivegega@gmail.com>
2018-10-22 23:40:32 -0300

    saco funciones de crear mensajes para MDJ

--- ensalada/ensalada/mensaje.h
+++ ensalada/ensalada/mensaje.h
@@ -51,46 +51,4 @@ DatoParticionado particionar_dato(void*, int, int);
 int enviar_mensaje(MensajeDinamico*);
 MensajeDinamico* recibir_mensaje(int);
 
-/*
- * @NAME: crear_mensaje_mdj_validar_archivo
- * @DESC: crea el mensaje para enviar request a mdj para validar un archivo
- * @ARG: socket de mdj + path del archivo
- */
-MensajeDinamico* crear_mensaje_mdj_validar_archivo(int socket_destino, char* path);
-
-/*
- * @NAME: crear_mensaje_mdj_crear_archivo
- * @DESC: crea el mensaje para enviar request a mdj para crear un archivo
- * @ARG: socket de mdj + path del archivo
- */
-MensajeDinamico* crear_mensaje_mdj_crear_archivo(int socket_destino, char* path, int cantidad_lineas);
-
-/*
- * @NAME: crear_mensaje_mdj_obtener_datos
- * @DESC: crea el mensaje para enviar request a mdj para obtener datos
- * @ARG: socket de mdj
- * 		+ path del archivo
- * 		+ offset: desde donde leer
- *		+ size: tamanio a leer
- */
-MensajeDinamico* crear_mensaje_mdj_obtener_datos(int socket_destino, char* path, int offset, int size);
-
-/*
- * @NAME: crear_mensaje_mdj_guardar_datos
- * @DESC: crea el mensaje para enviar request a mdj para guardar datos
- * @ARG: socket de mdj
- * 		+ path del archivo
- * 		+ offset: desde donde leer
- *		+ size: tamanio a leer
- *		+ buffer: datos a guardar
- */
-MensajeDinamico* crear_mensaje_mdj_guardar_datos(int socket_destino, char* path, int offset, int size, char* buffer);
-
-/*
- * @NAME: crear_mensaje_mdj_borrar_archivo
- * @DESC: crea el mensaje para enviar request a mdj para borrar un archivo de mdj
- * @ARG: socket de mdj + path del archivo
- */
-MensajeDinamico* crear_mensaje_mdj_borrar_archivo(int socket_destino, char* path);
-
 #endif //MENSAJE_H

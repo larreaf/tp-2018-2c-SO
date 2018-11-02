59de303018664007cdb106f03c5495b0a1808224
santi871 <santivegega@gmail.com>
2018-11-02 17:36:28 -0300

    agrego imprimir estado de pcp

--- sAFA/consola.c
+++ sAFA/consola.c
@@ -3,6 +3,7 @@
 
 extern bool correr;
 extern PLP* plp;
+extern PCP* pcp;
 
 void* consola_safa(void* arg){
 	while(correr){
@@ -109,7 +110,7 @@ tipo_accion_consola_safa string_to_accion(char* string){
 
 
 void con_ejecutar(char* ruta_escriptorio){
-	// TODO chequear que no este ya abierto el script
+
     DTB* nuevo_dtb = crear_dtb(contador_id_dtb++, 1);
 	log_info(plp->logger, "Creando DTB %d", nuevo_dtb->id);
 	string_append(&nuevo_dtb->path_script, ruta_escriptorio);
@@ -121,7 +122,8 @@ void con_ejecutar(char* ruta_escriptorio){
 
 void con_status(int id_DTB){
     if(id_DTB == -1){
-        imprimir_estado(plp);
+        imprimir_estado_plp(plp);
+        imprimir_estado_pcp(pcp);
     }
 	return;
 }

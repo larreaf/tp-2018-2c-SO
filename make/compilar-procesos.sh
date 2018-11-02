d0fc1067124634eea3993e12b10def901e7843bb
Federico Larrea <flarrea@est.frba.utn.edu.ar>
2018-11-02 17:35:21 -0300

    fix script de compilacion

--- make/compilar-procesos.sh
+++ make/compilar-procesos.sh
@@ -4,12 +4,10 @@ declare -a arr=("ensalada" "mdj" "fm9" "cpu" "elDiego" "sAFA")
 
 for i in "${arr[@]}"
 do
-    cd ../"$1"
-    mkdir Debug
-    cd ../make
     cp -r ./"$i"/Debug ../"$i"
     cd ../"$i"/Debug
     make
     cd ../../make 
 done
-
+"export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/workspace/tp-2018-2c-Ensalada-C-sar/ensalada/Debug" >> ~/.bashrc
+source ~/.bashrc

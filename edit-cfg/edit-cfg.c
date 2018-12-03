ef20bfae5656c6fee6b684841b95eae79b087558
Federico Larrea <flarrea@est.frba.utn.edu.ar>
2018-12-03 15:36:38 -0300

    Subo un programa para cambiar valores a las cfg

---
+++ edit-cfg/edit-cfg.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <commons/string.h>
+#include <commons/config.h>
+
+void validar_parametros(int argc){
+	if(argc < 3){
+		printf("La cantidad de parametros es incorrecta!\n");
+		exit(1);
+	}
+}
+
+int main(int argc, char **argv) {
+	t_config* configuracion = config_create(argv[1]);
+	if(config_has_property(configuracion,argv[2])){
+		config_set_value(configuracion,argv[2],argv[3]);
+		config_save(configuracion);
+		printf("%s seteado a: %s\n",argv[2],argv[3]);
+	}else {
+		printf("El archivo no tiene la propiedad: %s\n",argv[2]);
+	}
+	config_destroy(configuracion);
+	return EXIT_SUCCESS;
+}
